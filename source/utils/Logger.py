b9e2dd4201403458d0377ccacf6f077436bc657c
Hirokazu Kiyomaru <kiyomaru.tennis927@gmail.com>
2016-10-31 18:46:03 +0900

    update logger

--- source/utils/Logger.py
+++ source/utils/Logger.py
@@ -9,12 +9,14 @@ class Logger:
     def __init__(self, epoch):
         self.n_epoch = epoch
         self.prepare_graph()
-        self.current_loss = [0, 0]
         self.current_rec_loss = [0, 0]
+        self.current_reg_loss = [0, 0]
 
     def prepare_graph(self):
         self.y_train = np.zeros(self.n_epoch)
         self.y_test = np.zeros(self.n_epoch)
+        self.reg_y_train = np.zeros(self.n_epoch)
+        self.reg_y_test = np.zeros(self.n_epoch)
         self.rec_y_train = np.zeros(self.n_epoch)
         self.rec_y_test = np.zeros(self.n_epoch)
 
@@ -38,29 +40,33 @@ class Logger:
 
     def print_epoch(self, epoch):
         self.epoch = epoch
-        self.epoch_loss = [0, 0]
+        self.epoch_reg_loss = [0, 0]
         self.epoch_rec_loss = [0, 0]
         print "epoch:", epoch
 
-    def save_loss(self, loss, rec_loss, train=True):
+    def save_loss(self, reg_loss, rec_loss, train=True):
         if train: index = 0
         else: index = 1
-        self.current_loss[index] = loss
+        self.current_reg_loss[index] = reg_loss
         self.current_rec_loss[index] = rec_loss
-        self.epoch_loss[index] += self.current_loss[index]
+        self.epoch_reg_loss[index] += self.current_reg_loss[index]
         self.epoch_rec_loss[index] += self.current_rec_loss[index]
 
     def epoch_end(self):
-        self.y_train[self.epoch] = self.epoch_loss[0]
+        self.reg_y_train[self.epoch] = self.epoch_reg_loss[0]
+        self.reg_y_test[self.epoch] = self.epoch_reg_loss[1]
         self.rec_y_train[self.epoch] = self.epoch_rec_loss[0]
-        self.y_test[self.epoch] = self.epoch_loss[1]
         self.rec_y_test[self.epoch] = self.epoch_rec_loss[1]
+        self.y_train[self.epoch] = self.epoch_reg_loss[0] + self.epoch_rec_loss[0]
+        self.y_test[self.epoch] = self.epoch_reg_loss[1] + self.epoch_rec_loss[1]
 
         print "TRAIN"
-        print "# mean loss:", self.epoch_loss[0]
+        print "# mean loss:", self.epoch_reg_loss[0] + self.epoch_rec_loss[0]
+        print "# mean regularization loss:", self.epoch_reg_loss[0]
         print "# mean reconstruction loss:", self.epoch_rec_loss[0]
         print "TEST"
-        print "# mean loss:", self.epoch_loss[1]
+        print "# mean loss:", self.epoch_reg_loss[1] + self.epoch_rec_loss[1]
+        print "# mean regularization loss:", self.epoch_reg_loss[1]
         print "# mean reconstruction loss:", self.epoch_rec_loss[1]
 
     def terminate(self):
@@ -68,9 +74,9 @@ class Logger:
         self.show_loss_graph()
 
     def show_loss_graph(self):
-        y = np.asarray([self.y_train, self.y_test, self.rec_y_train, self.rec_y_test])
+        y = np.asarray([self.y_train, self.y_test, self.reg_y_train, self.reg_y_test, self.rec_y_train, self.rec_y_test])
         y = y.T
-        df = pd.DataFrame(y, columns=['y_train', 'y_test', 'rec_t_train', 'rec_y_test'])
+        df = pd.DataFrame(y, columns=['loss_train', 'loss_test', 'reg_loss_train', 'reg_loss_test', 'rec_loss_train', 'rec_loss_test'])
         df.plot(title='loss value transition')
         plt.show()
 
