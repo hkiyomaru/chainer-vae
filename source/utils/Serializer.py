f644b82f97cdeb8acfaab8658d5c57ff699a4c43
Hirokazu Kiyomaru <kiyomaru.tennis927@gmail.com>
2016-10-31 20:27:18 +0900

    add loader

--- source/utils/Serializer.py
+++ source/utils/Serializer.py
@@ -7,5 +7,8 @@ class Serializer:
         self.out_model_dir = out_model_dir
 
     def save(self, model, state, epoch):
-        serializers.save_hdf5("%s/model_ie_%d.h5"%(self.out_model_dir, epoch), model)
-        serializers.save_hdf5("%s/state_ie_%d.h5"%(self.out_model_dir, epoch), state)
+        serializers.save_hdf5("%s/model_%d.h5"%(self.out_model_dir, epoch), model)
+        serializers.save_hdf5("%s/state_%d.h5"%(self.out_model_dir, epoch), state)
+
+    def load(self, model, epoch):
+        serializers.load_hdf5("%s/model_%d.h5"%(self.out_model_dir, epoch), model)
